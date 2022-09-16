a3fe57e259bececbabbbd2df8bcffa640695ebf1
caiwei <1258062975@qq.com>
2022-09-16 15:14:04 +0800

    修改deploy.sh 执行

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -6,11 +6,14 @@ set -e
 # 生成静态文件
 npm run build
 
+- name: setup aliyun oss
 uses: yuekesoft/setup-ossutil@master
 with:
 endpoint: oss-cn-hangzhou.aliyuncs.com
 access-key-id: ${{ secrets.OSS_KEY_ID }}
 access-key-secret: ${{ secrets.OSS_KEY_SECRET }}
 
+- name: rm files from aliyun
 ossutil rm oss://vuepressdocs/ -rf
+- name: cp files to aliyun
 ossutil cp -rf .vuepress/dist  oss://vuepressdocs/
